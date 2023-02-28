422847d5658c7209d0c513697a49a830bd51d5d5
kollydap <dapkolly@gmail.com>
2023-02-28 13:16:32 +0100

    worked on settings

--- chat/settings.py
+++ chat/settings.py
@@ -125,9 +125,9 @@ CORS_ALLOWED_ORIGINS = [
   
 ]
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES':[
+    #     'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    # ],
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'rest_framework.authentication.TokenAuthentication',
     )  
